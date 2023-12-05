6201d23b062c8bfdd87ff74221d8f5718309fe9e
Marina Myssak <marina.myssak@outlook.com>
2023-12-05 08:29:58 +0600

    complite excuse-generator in javaScript

--- src/app.js
+++ src/app.js
@@ -4,8 +4,25 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
+const excuse = document.getElementById("excuse");
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let string = [];
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoExcuse = Math.floor(Math.random() * who.length);
+  let actionExcuse = Math.floor(Math.random() * action.length);
+  let whatExcuse = Math.floor(Math.random() * what.length);
+  let whenExcuse = Math.floor(Math.random() * when.length);
+  string = `${who[whoExcuse]} ${action[actionExcuse]} ${what[whatExcuse]} ${when[whenExcuse]}`;
+  excuse.innerText = string;
 };
